57f563506a406ad31c3ac8a229cd2d38c8912659
Genius-PAK <pyj789123@gmail.com>
2023-11-27 20:40:06 +0900

    feat: immigrate test code from Console to test directory

---
+++ mobile-shop/src/test/java/mobile/shop/holub/database/jdbc/ConsoleTest.java
@@ -0,0 +1,10 @@
+package mobile.shop.holub.database.jdbc;
+
+import mobile.shop.holub.jdbc.Console;
+
+class ConsoleTest {
+
+    public static void main(String[] args) throws Exception {
+        new Console();
+    }
+}
