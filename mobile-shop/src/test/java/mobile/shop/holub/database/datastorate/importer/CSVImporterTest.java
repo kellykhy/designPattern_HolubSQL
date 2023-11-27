99e55322ea549b1cb7c12e039b7bff739dc23599
Genius-PAK <pyj789123@gmail.com>
2023-11-27 21:17:09 +0900

    feat: add test for CSVImporter class

---
+++ mobile-shop/src/test/java/mobile/shop/holub/database/datastorate/importer/CSVImporterTest.java
@@ -0,0 +1,53 @@
+package com.test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.holub.database.datastorate.importer.CSVImporter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CSVImporterTest {
+    CSVImporter importer;
+
+    @BeforeEach
+    void init() throws IOException {
+        Reader in = new FileReader("people");
+        importer = new CSVImporter(in);
+        importer.startTable();
+    }
+
+    @Test
+    void testLoadTableName() throws IOException {
+        String tableNme = importer.loadTableName();
+        assertEquals("people", tableNme);
+    }
+
+    @Test
+    void testLoadColumns() throws IOException {
+        Iterator columnNames = importer.loadColumnNames();
+        String last = (String) columnNames.next();
+        assertEquals("last", last);
+        String first = (String) columnNames.next();
+        assertEquals("first", first);
+        String addrId = (String) columnNames.next();
+        assertEquals("addrId", addrId);
+    }
+
+    @Test
+    void testLoadRows() throws IOException {
+        Iterator row = importer.loadRow();
+        String last = (String) row.next();
+        assertEquals("Holub", last);
+        String first = (String) row.next();
+        assertEquals("Allen", first);
+        String addrId = (String) row.next();
+        assertEquals("1", addrId);
+    }
+
+
+}
