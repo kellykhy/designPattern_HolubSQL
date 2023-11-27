0d337bef02106bc0d8db333364e55da89da7766c
Genius-PAK <pyj789123@gmail.com>
2023-11-27 20:36:47 +0900

    feat: add FilePath class to get consistent base path according to OS

---
+++ mobile-shop/src/main/java/mobile/shop/holub/tools/FilePath.java
@@ -0,0 +1,29 @@
+package mobile.shop.holub.tools;
+
+public class FilePath {
+    public static String resourceFilePath = "c:\\dp2023";
+    public static String dbPath;
+
+    static {
+//        System.out.println(resourceFilePath);
+//        System.out.println(getDBPath(resourceFilePath));
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        if (osName.contains("mac")) {
+            resourceFilePath = System.getProperty("user.dir") + "/resources";
+//            System.out.println(resourceFilePath);
+        }
+        dbPath = getDBPath(resourceFilePath);
+
+
+    }
+
+    public static String getDBPath(String databaseName) {
+        String path = "file:/" + databaseName;
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            path = "file://" + databaseName;
+        }
+        return path;
+    }
+
+}
