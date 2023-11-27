790fef915ff28cde10f3c3423a10cbb0cb8c9b77
Genius-PAK <pyj789123@gmail.com>
2023-11-27 20:39:10 +0900

    feat: immigrate test code from JTableExporter to test directory

---
+++ mobile-shop/src/test/java/mobile/shop/holub/database/datastorate/exporter/JTableExporterTest.java
@@ -0,0 +1,31 @@
+package mobile.shop.holub.database.datastorate.exporter;
+
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import mobile.shop.holub.datastorage.exporter.JTableExporter;
+import mobile.shop.holub.datastorage.table.Table;
+import mobile.shop.holub.datastorage.table.TableFactory;
+
+class JTableExporterTest {
+    public static void main(String[] args) throws IOException {
+        Table people = TableFactory.create("resources/people",
+                new String[]{"First", "Last"});
+        people.insert(new String[]{"Allen", "Holub"});
+        people.insert(new String[]{"Ichabod", "Crane"});
+        people.insert(new String[]{"Rip", "VanWinkle"});
+        people.insert(new String[]{"Goldie", "Locks"});
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTableExporter tableBuilder = new JTableExporter();
+        people.export(tableBuilder);
+
+        frame.getContentPane().add(
+                new JScrollPane(tableBuilder.getJTable()));
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
