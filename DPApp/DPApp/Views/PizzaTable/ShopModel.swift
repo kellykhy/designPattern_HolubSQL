a8c6978281831caea6293590faf43351ecf7ba6a
JEONG-J <ikejhc159@gmail.com>
2023-11-30 19:54:55 +0900

    Feat :API 수정

--- DPApp/DPApp/Views/PizzaTable/ShopModel.swift
+++ DPApp/DPApp/Views/PizzaTable/ShopModel.swift
@@ -9,18 +9,10 @@ import Foundation
 
 struct RestaurantResponse: Codable {
     var tableName: String
-    var recodrs: [Restaurant]
+    var records: [Restaurant]
 }
 
 struct Restaurant: Codable {
     var id: String
     var name: String
-}
-
-class RestaurantInfo {
-    let restaurantInfo: [Restaurant] = [
-    Restaurant(id: "0", name: "도미노 피자"),
-    Restaurant(id: "1", name: "피자헛"),
-    Restaurant(id: "2", name: "미스터 피자"),
-    ]
 }
